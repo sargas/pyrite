716151acfe8e7bd879fd575cb13059489a3306f2
Joseph Jon Booker <joe@neoturbine.net>
2015-12-11 16:37:48 -0500

    Setup docs building and tests

---
+++ pyrite/__init__.py
@@ -0,0 +1,11 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+This package concerns the use of shapes and regions used by DS9, CIAO,
+and other astronomical software.
+"""
+
+from ._astropy_init import *
+
+if not _ASTROPY_SETUP_:
+    pass
